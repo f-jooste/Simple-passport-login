c2076b667df538b799b19f0f87f73d5f35f6e021
f-jooste <fourie.j789@gmail.com>
2026-05-18 06:46:20 +0200

    ADD: login functionality

--- passport-config.js
+++ passport-config.js
@@ -1,25 +1,26 @@
-const localStrategy =  require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const LocalStrategy =  require('passport-local').Strategy;
+const bcrypt = require('bcrypt')
 
-function initialize(passport, getUserByEmail) {
-    const authenticateUser = (email, password, done) => {
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async (email, password, done) => {
         const user = getUserByEmail(email);
         if (user == null) {
             return done(null, false, {message: 'No user with that email'})
         }
+        
         try {
             if (await bcrypt.compare(password, user.password)) {
                 return done(null, user)
             } else {
-                return done (null, flase, {message: 'Password inccorect'})
+                return done (null, false, {message: 'Password incorrect'})
             }
         } catch (err) {
             return done(err)
         }
     }
-    passport.use( new LocalStrategy({usernameField: 'emai'}), authenticateUser)
-    passport.serializeUser((user, done) => done())
-    passport.deserializeUser((is, done) => done())
+    passport.use( new LocalStrategy({usernameField: 'email'}, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => done(null, getUserById(id)))
 }
 
 module.exports = initialize; 
