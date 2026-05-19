f3edb71f7f69e4fea584dc399e2e3ba0fb3b7062
f-jooste <fourie.j789@gmail.com>
2026-05-19 20:22:54 +0200

    ADD: Function to prevent authenticated users from being authenticated again

--- server.js
+++ server.js
@@ -34,21 +34,21 @@ app.get('/', checkAuthenticated, (req, res) => {
     res.render('index.ejs', {name: req.user.name})
 })
 
-app.get('/login', (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
 })
 
-app.post('/login', passport.authenticate('local', {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true
 }))
 
-app.get('/register', (req, res) => {
+app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', async(req, res) => {
+app.post('/register', checkNotAuthenticated, async(req, res) => {
     try {
         const hashedPassword = bcrypt.hash(req.body.password, 10);
         users.push({
@@ -71,6 +71,13 @@ function checkAuthenticated(req, res, next) {
     }
 
     res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        res.redirect('/')
+    }
+    next()
 }
 
 app.listen(8000);
